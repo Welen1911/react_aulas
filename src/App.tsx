8ca566cf010ce466202579bae009fd6ee273d95d
Welen1911 <welenalmeida2210@gmail.com>
2023-06-04 21:08:55 -0300

    Apagando e modificando arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Escreva seu nome aqui embaixo:
-        </p>
-        <input type='text' placeholder='Nome'></input> <br />
-        <input type="number" /> <br />
-        <textarea name="" id=""></textarea>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
-export default App;
