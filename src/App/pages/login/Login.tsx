198d686d52963449a401c9817de43f8ab9eeacee
Welen1911 <welenalmeida2210@gmail.com>
2023-06-11 20:50:53 -0300

    Utilizando useEffect

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
+import { isSet } from "util/types";
 
 export const Login = () => {
     const history = useNavigate();
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState(""); 
+    
+    useEffect(() => {
+       console.log(email);
+    }, [email]);
+
+    useEffect(() => {
+        console.log(senha);
+     }, [senha]);
+    
+    
     const handleEntrar = () => { 
+
         let h1 = document.createElement("h1");
         h1.innerHTML = `O seu E-mail é ${email}`;
+        h1.id = "h1";
         document.getElementById("div")?.appendChild(h1);
 
         let h2 = document.createElement("h2");
         h2.innerHTML = "A sua senha é "+senha;
+        h2.id = "h2";
         document.getElementById("div")?.appendChild(h2);
-    // history("/pagina-inicial");
+        
+        h1.innerHTML = `O seu E-mail é ${email}`;
+        h2.innerHTML = "A sua senha é "+senha;
+       
+    //  history("/pagina-inicial");
     }
     return (
         <div id="div">
